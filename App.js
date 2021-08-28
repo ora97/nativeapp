14519cd651947dc7554c93f25f880721f7da6c7f
ope <oranimashaun@gmail.com>
2021-08-28 00:34:33 -0700

    Components Part 2

--- App.js
+++ App.js
@@ -4,5 +4,5 @@ import Main from './components/MainComponent'
 export default function App() {
   return (
     <Main />
-  );
+  )
 }
--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
 
@@ -10,7 +10,8 @@ function Directory(props) {
             <ListItem
                 title={item.name}
                 subtitle={item.description}
-                leftAvatar={{source: require('./images/react-lake.jpg')}}
+                onPress={() => props.onPress(item.id)}
+                leftAvatar={{ source: require('./images/react-lake.jpg')}}
             />
         )
     };
